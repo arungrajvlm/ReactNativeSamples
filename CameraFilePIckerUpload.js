56eedd90442b2f589185ad627db3e7812550c68a
ARUN G RAJ <37746931+arungrajvlm@users.noreply.github.com>
2019-07-19 20:10:55 +0530

    AndroidManifest notes added
    Set the below permission in AndroidManifest File
      <uses-permission android:name="android.permission.INTERNET" />
        <uses-permission android:name="android.permission.CAMERA"/>
        <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>

--- CameraFilePIckerUpload.js
+++ CameraFilePIckerUpload.js
@@ -200,3 +200,10 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+/*
+Set the below permission in AndroidManifest File
+  <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.CAMERA"/>
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+*/
